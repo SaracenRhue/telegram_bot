ffa7c8cce557f71e035e6f0d9b74bc0aee4c7f70
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-23 15:49:22 +0200

    edit

--- keyword_response.py
+++ keyword_response.py
@@ -12,14 +12,12 @@ with open('config.yml', 'r') as file:
 # respond to tower commands
 async def keyword_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
-    wrap_use_tower_shell_exists = True
     async def wrap_use_tower_shell(text):
         text = text.replace('$', '')
         await random_working_text(update, context)
         use_tower_shell(text)
         await random_done_text(update, context)
 
-    wrap_random_stash_video_exists = True
     async def wrap_random_stash_video():
         await random_working_text(update, context)
         await send_text(random_stash_video(), update, context)
@@ -29,12 +27,12 @@ async def keyword_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 ####################################################################################################        
 # if no wrap_ function excists, random_working_text() and random_done_text() will be used by default
-# if you create a wrap_ function you need to set the _exists variable to True
+# if you create a wrap_ function you need to set the default to False in the config.yml
     text = update.message.text.lower()
     for i in messages:
         if len(messages[i]['trigger']) == 3:
             if messages[i]['trigger'][0] in text and messages[i]['trigger'][1] in text or messages[i]['trigger'][2] in text:
-                if 'wrap_'+messages[i]['function']+'_exists' == True:
+                if messages[i]['default'] == False:
                     await eval('wrap_'+messages[i]['function'])
                 else:
                     await random_working_text(update, context)
@@ -43,7 +41,7 @@ async def keyword_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 return True
         elif len(messages[i]['trigger']) == 2:
             if messages[i]['trigger'][0] in text and messages[i]['trigger'][1] in text:
-                if 'wrap_'+messages[i]['function']+'_exists' == True:
+                if messages[i]['default'] == False:
                     await eval('wrap_'+messages[i]['function'])
                 else:
                     await random_working_text(update, context)
@@ -52,7 +50,7 @@ async def keyword_response(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 return True
         elif len(messages[i]['trigger']) == 1:
             if messages[i]['trigger'][0] in text:
-                if 'wrap_'+messages[i]['function']+'_exists' == True:
+                if messages[i]['default'] == False:
                     await eval('wrap_'+messages[i]['function'])
                 else:
                     await random_working_text(update, context)
